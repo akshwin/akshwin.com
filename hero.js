aed746d7b2bbfbc2e073a502fba27b729f6b96c2
akshwin tamilselvan <akshwint.2003@gmail.com>
2025-04-29 18:30:55 +0530

    fixed footer

--- hero.js
+++ hero.js
@@ -5,16 +5,27 @@ const navbar = document.querySelector('.navbar');
 navbar.style.display = 'block';
 
 // Handle navbar visibility on scroll
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   if (window.scrollY === 0) {
     navbar.style.display = 'block'; // Show navbar at the top
   } else {
     navbar.style.display = 'none'; // Hide navbar when scrolling down
   }
+
+  // Footer visibility on scroll end
+  const footer = document.getElementById('footer');
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const pageHeight = document.documentElement.scrollHeight;
+
+  if (scrollPosition >= pageHeight - 10) {
+    footer.classList.add('visible'); // Show footer
+  } else {
+    footer.classList.remove('visible'); // Hide footer
+  }
 });
 
 // Typed.js Initialization after DOM is fully loaded
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   new Typed('#typed', {
     strings: [
       "Data Science Enthusiast",
